9995fe0ce877586154d14754691bcf744fe905a3
Elena Swiderskaya <olenyonock7@rambler.ru>
2022-11-20 18:24:40 +0300

    pictures refactoring

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,12 +1,14 @@
 import classes from './Post.module.css'
+import smile from '../../../../assets/images/face_smile.png';
+import likeIcon from '../../../../assets/icons/like.jpg';
 
 const Post = (props) => {
   return (
     <div className = {classes.post_container}>
-      <img alt="avatar" src="https://cs6.pikabu.ru/avatars/2097/x2097369-1271064885.png" />
+      <img alt="avatar" src={smile} />
       <div>{props.text}</div>
       <div className={classes.info_container}>
-        <img src="https://banner2.cleanpng.com/20180429/tew/kisspng-facebook-like-button-computer-icons-social-network-5ae5bb33a392a7.15157648152500510767.jpg" alt="like" />
+        <img src={likeIcon} alt="like" />
         <span>{props.likesCount}</span>
       </div>
     </div>
