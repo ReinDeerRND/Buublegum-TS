95f310232ba683201046548558b30fee2e047358
Elena Swiderskaya <olenyonock7@rambler.ru>
2023-01-17 20:41:57 +0300

    fix test

--- src/components/Profile/ProfileInfo/ProfileStatus.test.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.test.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import { create } from 'react-test-renderer';
+import { create, ReactTestInstance } from 'react-test-renderer';
 import ProfileStatus from './ProfileStatus';
 
+//todo: не все тесты работают - из-за замены expect(instance...)  на expect(instance?.instance ...)
 describe("Profile Status Component", () => {
     test("it should show status from props in component", () => {
         const status = 'status from props';
-        const component = create(<ProfileStatus status={status} />);
-        const instance = component.getInstance();
-        expect(instance.state.status).toBe(status);
+        const component = create(<ProfileStatus status={status} updateStatus={x =>{}} />);
+        const instance: ReactTestInstance | null = component.getInstance();
+        expect(instance?.instance.state.status).toBe(status);
     });
 
     test("it should be span during non-edit mode", () => {
-        const component = create(<ProfileStatus status="some status" />);
+        const component = create(<ProfileStatus status="some status" updateStatus={x =>{}}  />);
         const root = component.root;
         let span = root.findByType('span');
         expect(span).toBeTruthy();
     });
 
     test("it should not be input during non-edit mode", () => {
-        const component = create(<ProfileStatus status="some status" />);
+        const component = create(<ProfileStatus status="some status" updateStatus={x =>{}} />);
         const root = component.root;
         expect(()=>{
             let input = root.findByType('input');
@@ -27,7 +28,7 @@ describe("Profile Status Component", () => {
 
     test("span text must be equal as props status", () => {
         const status = 'status from props';
-        const component = create(<ProfileStatus status={status} />);
+        const component = create(<ProfileStatus status={status} updateStatus={x =>{}} />);
         const root = component.root;
         let span = root.findByType('span');
         expect(span.children).toContain(status);
@@ -35,7 +36,7 @@ describe("Profile Status Component", () => {
 
     test("it should be input during edit mode and editMode should be true", () => {
         const status = 'status from props';
-        const component = create(<ProfileStatus status={status} />);
+        const component = create(<ProfileStatus status={status} updateStatus={x =>{}} />);
         const root = component.root;
         const instance = component.getInstance();
         let span = root.findByType('span');
@@ -43,14 +44,14 @@ describe("Profile Status Component", () => {
         let input = root.findByType('input');
         expect(input).toBeTruthy();
         expect(input.props.value).toBe(status);
-        expect(instance.state.editMode).toBeTruthy();
+        expect(instance?.instance.state.editMode).toBeTruthy();
     });
 
     test("callback 'updateStatus' should be called", () => {
         const updateStatusMock = jest.fn();
         const component = create(<ProfileStatus status= '' updateStatus = {updateStatusMock}/>);
         const instance = component.getInstance();
-        instance.deactivateEditMode();
+        instance?.instance.deactivateEditMode();
         expect(updateStatusMock.mock.calls.length).toBe(1);
     });
 })
